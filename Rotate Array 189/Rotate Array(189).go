e6420d7638846c8972f277b1b37e7a4946156046
Timur <111347232+timssDre@users.noreply.github.com>
2023-01-11 00:33:31 +0600

    1

--- Rotate Array 189/Rotate Array(189).go
+++ Rotate Array 189/Rotate Array(189).go
@@ -1,14 +1,30 @@
 package main
 
-import "fmt"
-
 func main() {
-	var nums = []int{1, 2, 3, 4, 5, 6, 7}
-	k := 3
-	fmt.Println(nums, k)
+	//var nums = []int{1, 2, 3, 4, 5, 6, 7}
+	//k := 3
+
+	//var nums = []int{-1}
+	//k := 2
+
+	//var nums = []int{1, 2}
+	//k := 2
+
+	var nums = []int{1, 2, 3}
+	k := 4
+	rotate(nums, k)
 }
+
 func rotate(nums []int, k int) {
-	first := nums[:k]
-	second := nums[k:]
-	fmt.Println(first, second)
+	lNums := len(nums)
+	first := nums[:lNums-k]
+	second := nums[lNums-k:]
+
+	seven := make([]int, lNums)
+	s := seven[:0]
+	s = append(s, second[:]...)
+	s = append(s, first[:]...)
+	for i, value := range seven {
+		nums[i] = value
+	}
 }
