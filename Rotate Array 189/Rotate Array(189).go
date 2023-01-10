410cbf2a6ee36b0bebb5a6db092ee1fb1c9fd586
Timur <111347232+timssDre@users.noreply.github.com>
2023-01-11 00:53:21 +0600

    1

--- Rotate Array 189/Rotate Array(189).go
+++ Rotate Array 189/Rotate Array(189).go
@@ -8,15 +8,38 @@ func main() {
 	//k := 2
 
 	//var nums = []int{1, 2}
+	//k := 3
+
+	var nums = []int{1}
+	k := 1
+
+	//var nums = []int{1, 2}
 	//k := 2
 
-	var nums = []int{1, 2, 3}
-	k := 4
+	//var nums = []int{1, 2, 3}
+	//k := 4
+
+	//var nums = []int{1, 2, 3, 4, 5, 6, 7}
+	//k := 3
+
 	rotate(nums, k)
 }
 
 func rotate(nums []int, k int) {
 	lNums := len(nums)
+	if lNums == 1 {
+		return
+	} else if lNums <= k {
+		aDifference := lNums - k
+		a := k - (1 - aDifference)
+		recursion(nums, a, lNums)
+		k -= a
+		rotate(nums, k)
+	} else {
+		recursion(nums, k, lNums)
+	}
+}
+func recursion(nums []int, k int, lNums int) {
 	first := nums[:lNums-k]
 	second := nums[lNums-k:]
 
